d61ab97b241d40b591f6d900d25f3d747204f73b
Light505 <181879156+Light505@users.noreply.github.com>
2026-05-14 17:13:24 +0800

    chore: update src/core/initialize.php

--- src/core/initialize.php
+++ src/core/initialize.php
@@ -6,12 +6,12 @@
     //defines DS as directory separator...essentially \ on windows and / on linux, so that the code can be used on both systems without modification
     defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
-    //resolve the app root where api/, core/, and include/ directories live
+    //resolve the src root where core/ and include/ directories live
     defined('ROOT') ? null : define('ROOT', dirname(__DIR__) . DS);
     
     //defines include and core paths from the resolved root
-    defined('INC_PATH') ? null : define('INC_PATH', ROOT . 'src' . DS . 'include' . DS);
-    defined('CORE_PATH') ? null : define('CORE_PATH', ROOT . 'src' . DS . 'core' . DS);
+    defined('INC_PATH') ? null : define('INC_PATH', ROOT . 'include' . DS);
+    defined('CORE_PATH') ? null : define('CORE_PATH', ROOT . 'core' . DS);
 
     //load db config 
     require_once(INC_PATH . 'config.php');
